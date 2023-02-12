08ccc022e868cf3456ed6a3eb9209e694b349165
loupdaniel <loupdaniel12@gmail.com>
2023-02-13 08:07:46 +0900

    12th save

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:secondlife_mobile/PageViewHolder.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -17,6 +18,16 @@ class _MyHomePageState extends State<MyHomePage> {
   double fraction =
       0.57; // By using this fraction, we're telling the PageView to show the 50% of the previous and the next page area along with the main page
 
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri(scheme: "https", host: url);
+    if (!await launchUrl(
+      uri,
+      mode: LaunchMode.inAppWebView,
+    )) {
+      throw 'Can not launch url';
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -28,6 +39,23 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   int index = 1;
+
+  int currentIndex = 0;
+
+  List<String> images = [
+    "https://i.ytimg.com/vi/PWADVtWyE9Q/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDcneFqOxHd28mCncQxT3jOErmk9Q",
+    "https://i.ytimg.com/vi/djzDWMy1z7k/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCHwD_IA2ERzpZVxNvxCEOGr4fyTw",
+    "https://i.ytimg.com/vi/n8OxyKNBsuQ/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAtW45_cxRqEWfUVw19UMts_9Q0lQ",
+    "https://i.ytimg.com/vi/7bDFD_WcU9I/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAsgAH6VRN4w0HKtVc528WA5QSZ2w",
+    "https://i.ytimg.com/vi/_ABk7TmjnVk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAxCeIml0HUbjJ3igi1FFe1esdwdg",
+    "https://i.ytimg.com/vi/-8m0XFea2zE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDBOBRGDJeDjhT1HbRobSN2Tp6hMA",
+    "https://i.ytimg.com/vi/mXLS2IzZSdg/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhdIFsoZTAP&rs=AOn4CLDS13MjaIBxjjhccIktpAb0azBG9g",
+    "https://i.ytimg.com/vi/HuzlYAMwwJY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCmfMS9RENZuIJMQ8k2cf6MbHIpug",
+    "https://i.ytimg.com/vi/-nt_u4vo-DI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAgUinltWhU-qqmgc_JroDLPt3OEg",
+    "https://i.ytimg.com/vi/tqtZIyN_Alg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD4woxvyiNXgmSile7PLz7uoRPQOQ",
+  ];
+
+  final PageController controller = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const SizedBox(
-                  height: 70,
+                  height: 30,
                 ),
                 const Padding(
                   padding: EdgeInsets.symmetric(horizontal: 35),
@@ -90,10 +118,92 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                 ),
-                //const SizedBox(height: 2.0),
+                Transform.translate(
+                  offset: const Offset(0, -85),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 35),
+                    child: Text(
+                      'Watch videos',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+
+                //https://www.youtube.com/watch?v=7a_RXHOkJLM
+                //https://github.com/Programmer9211/Flutter-Carousel-Slider/blob/main/lib/main.dart
+                Transform.translate(
+                  offset: const Offset(0, -65),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        child: SizedBox(
+                          height: 162,
+                          width: 335,
+                          child: PageView.builder(
+                            controller: controller,
+                            onPageChanged: (index) {
+                              setState(() {
+                                currentIndex = index % images.length;
+                              });
+                            },
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 35),
+                                child: SizedBox(
+                                  height: 100,
+                                  width: 400,
+                                  child: Image.network(
+                                    images[index % images.length],
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      /*const SizedBox(
+                        height: 20,
+                      ),*/
+                      /*Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          for (var i = 0; i < images.length; i++)
+                            buildIndicator(currentIndex == i)
+                        ],
+                      ),*/
+                      /*Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                controller.jumpToPage(currentIndex - 1);
+                              },
+                              icon: const Icon(Icons.arrow_back),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                controller.jumpToPage(currentIndex + 1);
+                              },
+                              icon: const Icon(Icons.arrow_forward),
+                            ),
+                          ],
+                        ),
+                      ),*/
+                    ],
+                  ),
+                ),
                 ////Your Playlist of the week text
                 Transform.translate(
-                  offset: const Offset(0, -70),
+                  offset: const Offset(0, -30),
                   child: const Padding(
                     padding: EdgeInsets.symmetric(horizontal: 35),
                     child: Text(
@@ -106,7 +216,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 Transform.translate(
-                  offset: const Offset(0, -40),
+                  offset: const Offset(0, -15),
                   child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 35),
                     child: SingleChildScrollView(
@@ -117,85 +227,85 @@ class _MyHomePageState extends State<MyHomePage> {
                             child: ListView(
                               scrollDirection: Axis.horizontal,
                               children: <Widget>[
-                                InkWell(
-                                  onTap: () {},
-                                  child: Ink(
-                                    child: SizedBox(
-                                      height: 160.0,
-                                      width: 200.0,
-                                      child: Image.asset(
-                                        'assets/images/album1.jpg',
-                                        height: 160.0,
-                                        width: 200.0,
-                                      ),
+                                GestureDetector(
+                                  onTap: () {
+                                    _launchURL("www.google.com");
+                                  },
+                                  child: SizedBox(
+                                    height: 180.0,
+                                    width: 220.0,
+                                    child: Image.asset(
+                                      'assets/images/album1.jpg',
+                                      height: 180.0,
+                                      width: 220.0,
                                     ),
                                   ),
                                 ),
                                 const SizedBox(
                                   width: 30,
                                 ),
-                                InkWell(
-                                  onTap: () {},
-                                  child: Ink(
-                                    child: SizedBox(
-                                      height: 160.0,
-                                      width: 200.0,
-                                      child: Image.asset(
-                                        'assets/images/album2.jpg',
-                                        height: 160.0,
-                                        width: 200.0,
-                                      ),
+                                GestureDetector(
+                                  onTap: () {
+                                    _launchURL("www.google.com");
+                                  },
+                                  child: SizedBox(
+                                    height: 180.0,
+                                    width: 220.0,
+                                    child: Image.asset(
+                                      'assets/images/album2.jpg',
+                                      height: 180.0,
+                                      width: 220.0,
                                     ),
                                   ),
                                 ),
                                 const SizedBox(
                                   width: 30,
                                 ),
-                                InkWell(
-                                  onTap: () {},
-                                  child: Ink(
-                                    child: SizedBox(
+                                GestureDetector(
+                                  onTap: () {
+                                    _launchURL("www.google.com");
+                                  },
+                                  child: SizedBox(
+                                    height: 160.0,
+                                    width: 200.0,
+                                    child: Image.asset(
+                                      'assets/images/album3.jpg',
                                       height: 160.0,
                                       width: 200.0,
-                                      child: Image.asset(
-                                        'assets/images/album3.jpg',
-                                        height: 160.0,
-                                        width: 200.0,
-                                      ),
                                     ),
                                   ),
                                 ),
                                 const SizedBox(
                                   width: 30,
                                 ),
-                                InkWell(
-                                  onTap: () {},
-                                  child: Ink(
-                                    child: SizedBox(
+                                GestureDetector(
+                                  onTap: () {
+                                    _launchURL("www.google.com");
+                                  },
+                                  child: SizedBox(
+                                    height: 160.0,
+                                    width: 200.0,
+                                    child: Image.asset(
+                                      'assets/images/album4.jpg',
                                       height: 160.0,
                                       width: 200.0,
-                                      child: Image.asset(
-                                        'assets/images/album4.jpg',
-                                        height: 160.0,
-                                        width: 200.0,
-                                      ),
                                     ),
                                   ),
                                 ),
                                 const SizedBox(
                                   width: 30,
                                 ),
-                                InkWell(
-                                  onTap: () {},
-                                  child: Ink(
-                                    child: SizedBox(
+                                GestureDetector(
+                                  onTap: () {
+                                    _launchURL("www.google.com");
+                                  },
+                                  child: SizedBox(
+                                    height: 160.0,
+                                    width: 200.0,
+                                    child: Image.asset(
+                                      'assets/images/album5.jpg',
                                       height: 160.0,
                                       width: 200.0,
-                                      child: Image.asset(
-                                        'assets/images/album5.jpg',
-                                        height: 160.0,
-                                        width: 200.0,
-                                      ),
                                     ),
                                   ),
                                 ),
@@ -214,6 +324,20 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  /*Widget buildIndicator(bool isSelected) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 1),
+      child: Container(
+        height: isSelected ? 12 : 10,
+        width: isSelected ? 12 : 10,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: isSelected ? Colors.black : Colors.grey,
+        ),
+      ),
+    );
+  }*/
 }
 
 class MyPage extends StatelessWidget {
